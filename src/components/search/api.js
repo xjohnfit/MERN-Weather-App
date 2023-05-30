3d5a5825e6798292a9581bcbcd108c17cb20071c
John Rocha <xjohnfitcodes@gmail.com>
2023-05-30 14:02:28 -0400

    first commit

---
+++ src/components/search/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '46e4600040msh2fc1496a0ac3011p147823jsn2ca935ec8b45',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6ca915994c44339e3dec399bd029bfbb";
